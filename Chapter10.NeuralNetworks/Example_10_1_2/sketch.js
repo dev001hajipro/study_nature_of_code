2a5d51e8cc5b289918c402ca482b8b7652716d95
dev001hajipro <dev001hajipro@gmail.com>
2017-09-21 09:20:26 +0900

    modify

---
+++ Chapter10.NeuralNetworks/Example_10_1_2/sketch.js
@@ -0,0 +1,115 @@
+/// <reference path="../../p5.global-mode.d.ts" />
+
+/////////////////////////////
+// 用語 
+/////////////////////////////
+// コネクショニズム - connectionism
+// 概念や常識を最初に持たず、白紙の状態から、多数の事例で"経験"させ、"学習"していく
+// 疑似ニューロンを作成する。
+//
+// 教育あり学習 - Supervised Learning
+// ネットワークが出した答えに対して、あらかじめ用意してある正しい答えと比較し、誤差を調整
+// 強化学習 - Reinforcement Learning
+// 環境の観察により精度を高めていく。迷路のコストなど。
+//
+// パーセプトロン - Perceptron
+// 最も単純なニューラルネットワークであり、単一ニューロンの計算モデル
+//
+
+/////////////////////////////
+// 10.3 パーセプトロンを使った簡単なパターン認識
+
+class Perceptron {
+    constructor(n) {
+        this.C_learingRate = 0.01;
+        this.weights = Array.from({length:n}, (v,k)=>random(-1.0, 1.0))
+        console.log('weights', this.weights)
+    }
+    // 単純なパーセプトロンのアルゴリズム
+    // 1.入力ごとに、入力と重みをかける
+    // 2.すべての入力を合計
+    // 3.活性化関数(activation function)に、合計値を渡し、出力を計算(推測)
+    feedforward(inputs) {
+         const zipped = inputs.map((v,i,a)=> v * this.weights[i]);
+         const sum = zipped.reduce((acc, c)=> acc + c)
+         return this.activate(sum);
+    }
+    activate(n) { return n > 0 ? 1 : -1; }
+
+    train(inputs, desired) {
+        // 入力から推測結果を求める。
+        let guess = this.feedforward(inputs);
+        let error = desired - guess; // 希望する答えと、推測結果を比較
+        // 重みの調整。推測結果(guess)が正しいとerror=0となり、重みは変化しない。
+        // [重みの式]
+        // 新しい重み = 重み + Δ重み
+        // Δ重み = エラー * 入力
+        // 新しい重み = 重み + エラー * 入力
+        // さらに、定数を用意
+        // 新しい重み = 重み + エラー * 入力 * 学習定数
+        this.weights.forEach((v,i,a)=> a[i] = a[i] + error * inputs[i] * this.C_learingRate);
+    }
+}
+
+const f = (x) => 2 * x + 1;
+
+class Trainer {
+    constructor(x, y, answer) {
+        this.inputs = [x, y, BIAS];
+        this.answer = answer;
+    }
+
+    static makeData(n) {
+        return Array.from({length:n}, (v,k)=> {
+            let x = random(-width/2, width/2);
+            let y = random(-height/2, height/2);
+            let answer = y < f(x) ? -1 : 1;
+            return new Trainer(x, y, answer)
+        });
+    }
+}
+
+const BIAS = 1.0
+
+let p;
+let training;
+
+function setup() {
+    createCanvas(400, 400);
+    p = new Perceptron(3); // 重み変数3,weight[3]
+    training = Trainer.makeData(2000);
+}
+
+let count = 0;
+function draw() {
+    background(240);
+
+    push();
+    translate(width/2, height/2);
+
+    // パーセプトロンの重みが学習されていく。
+    p.train(training[count].inputs, training[count].answer)
+
+    count = (count + 1) % training.length;
+
+    //const f = (x) => 2 * x + 1;
+    stroke(255, 0, 0)
+    line(-width/2, f(-width/2), width/2, f(width/2))
+
+    // 描画
+    stroke(0);
+    for (let i = 0; i < count; i++) {
+        let guess = p.feedforward(training[i].inputs);
+        (guess > 0) ? noFill() : fill(0);
+        ellipse(training[i].inputs[0], training[i].inputs[1], 8, 8);
+    }
+    pop();
+
+    push();
+    noStroke();
+    fill(255, 200);
+    rect(0, 0, 90, 30)
+    fill(0);
+    text(`COUNT:${count}`, 10, 20)
+    pop();
+}
--- Chapter10.NeuralNetworks/Example_10_2/sketch.js
+++ Chapter10.NeuralNetworks/Example_10_2/sketch.js
@@ -1,111 +1,100 @@
 /// <reference path="../../p5.global-mode.d.ts" />
 
-/////////////////////////////
-// 用語 
-/////////////////////////////
-// コネクショニズム - connectionism
-// 概念や常識を最初に持たず、白紙の状態から、多数の事例で"経験"させ、"学習"していく
-// 疑似ニューロンを作成する。
-//
-// 教育あり学習 - Supervised Learning
-// ネットワークが出した答えに対して、あらかじめ用意してある正しい答えと比較し、誤差を調整
-// 強化学習 - Reinforcement Learning
-// 環境の観察により精度を高めていく。迷路のコストなど。
-//
-// パーセプトロン - Perceptron
-// 最も単純なニューラルネットワークであり、単一ニューロンの計算モデル
-//
+const floor1=(n)=>floor(n*10)/10;
 
-/////////////////////////////
-// 10.3 パーセプトロンを使った簡単なパターン認識
-
-class Perceptron {
-    constructor(n) {
-        this.C_learingRate = 0.01;
-        this.weights = Array.from({length:n}, (v,k)=>random(-1.0, 1.0))
-        console.log('weights', this.weights)
+class Vehicle {
+    constructor(x = 0, y = 0, m = 1) {
+        this.maxspeed = 5;        
+        this.position = createVector(x, y);
+        this.velocity = createVector(random(-this.maxspeed, this.maxspeed),random(-this.maxspeed, this.maxspeed));
+        this.acceleration = createVector(0, 0);
+        this.mass = 1;
+        this.r = 5;
+        this.maxforce = 0.15;
     }
-    // 単純なパーセプトロンのアルゴリズム
-    // 1.入力ごとに、入力と重みをかける
-    // 2.すべての入力を合計
-    // 3.活性化関数(activation function)に、合計値を渡し、出力を計算(推測)
-    feedforward(inputs) {
-         const zipped = inputs.map((v,i,a)=> v * this.weights[i]);
-         const sum = zipped.reduce((acc, c)=> acc + c)
-         return this.activate(sum);
+    update() {
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.acceleration.mult(0);
     }
-    activate(n) { return n > 0 ? 1 : -1; }
-
-    train(inputs, desired) {
-        // 入力から推測結果を求める。
-        let guess = this.feedforward(inputs);
-        let error = desired - guess; // 希望する答えと、推測結果を比較
-        // 重みの調整。推測結果(guess)が正しいとerror=0となり、重みは変化しない。
-        // [重みの式]
-        // 新しい重み = 重み + Δ重み
-        // Δ重み = エラー * 入力
-        // 新しい重み = 重み + エラー * 入力
-        // さらに、定数を用意
-        // 新しい重み = 重み + エラー * 入力 * 学習定数
-        this.weights.forEach((v,i,a)=> a[i] = a[i] + error * inputs[i] * this.C_learingRate);
+    applyForce(force) {
+        // F=AM, A=F/M
+        let a = p5.Vector.div(force, this.mass);
+        this.acceleration.add(a);
     }
-}
+    seek(targets) {
+        targets.forEach(target=> {
+            // 操舵ベクトル=進みたい速度ベクトル-現在の速度ベクトル
+            let desired = p5.Vector.sub(target.position, this.position);
+            desired.normalize();
+            desired.mult(this.maxspeed);
+            let steer = p5.Vector.sub(desired, this.velocity);
 
-const f = (x) => 2 * x + 1;
+            // 距離によって重みを付ける
+            let d = p5.Vector.dist(target.position, this.position);
+            let weight = map(d, 0, width, 0, 0.5);
+            steer.mult(weight);
 
-class Trainer {
-    constructor(x, y, answer) {
-        this.inputs = [x, y, BIAS];
-        this.answer = answer;
-    }
-
-    static makeData(n) {
-        return Array.from({length:n}, (v,k)=> {
-            let x = random(-width/2, width/2);
-            let y = random(-height/2, height/2);
-            let answer = y < f(x) ? -1 : 1;
-            return new Trainer(x, y, answer)
+            steer.limit(this.maxforce);
+            this.applyForce(steer);
         });
     }
+    display() {
+        let h = this.velocity.heading() + PI/2;
+        push();
+        translate(this.position.x, this.position.y);
+        rotate(h);
+        scale(this.r);
+        stroke(0);
+        strokeWeight(1/this.r);
+        fill(255, 100);
+        beginShape();
+        vertex(0, -2);
+        vertex(-1, 2);
+        vertex(1, 2);
+        endShape(CLOSE);
+        // text
+        fill(0);
+        scale(1/this.r);
+        scale(2);
+        let _x = floor1(this.velocity.x);
+        let _y = floor1(this.velocity.y);
+        
+        text(`v(${_x},${_y})`, 0, 0);
+        pop();
+    }
 }
 
+class Target {
+    constructor() {
+        this.position = createVector(random(0, width), random(0, height));
+        this.mass = 1;
+    }
+    display() {
+        push();
+        translate(this.position.x, this.position.y);
+        fill(50, 100);
+        stroke(50);
+        ellipse(0, 0, this.mass*10, this.mass*10);
+        point(0, 0);
+        pop();
+    }
+}
 
-
-const BIAS = 1.0
-
-let p;
-let training;
-
+let vehicle;
+let targets;
 function setup() {
-    createCanvas(400, 400);
-    p = new Perceptron(3); // 重み変数3,weight[3]
-    training = Trainer.makeData(2000);
-    console.log(training)
+    createCanvas(700, 300);
+    vehicle = new Vehicle(width/2, height/2);
+    targets = Array.from({length:2}, (v,k)=> new Target());
+    console.log(targets)
 }
-
-let count = 0;
 function draw() {
     background(240);
-    translate(width/2, height/2);
+    vehicle.seek(targets);
+    //vehicle.bound(50);
+    vehicle.update();
+    vehicle.display();
 
-    // パーセプトロンの重みが学習されていく。
-    p.train(training[count].inputs, training[count].answer)
-
-    count = (count + 1) % training.length;
-
-    //const f = (x) => 2 * x + 1;
-    stroke(255, 0, 0)
-    line(-width/2, f(-width/2), width/2, f(width/2))
-
-    // 描画
-    stroke(0);
-    for (let i = 0; i < count; i++) {
-        let guess = p.feedforward(training[i].inputs);
-        if (guess > 0) {
-            noFill();
-        } else {
-            fill(0);
-        }
-        ellipse(training[i].inputs[0], training[i].inputs[1], 8, 8);
-    }
+    targets.forEach(v=>v.display());
 }
